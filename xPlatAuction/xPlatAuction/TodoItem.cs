6f3f26bf46856b9d07b89d453634ac1b9dab23e4
rakemen <rcmenon@indiana.edu>
2019-01-12 02:35:13 -0500

    Added DataObject 'ToDoItem' to Shared Client

---
+++ xPlatAuction/xPlatAuction/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace xPlatAuction
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
