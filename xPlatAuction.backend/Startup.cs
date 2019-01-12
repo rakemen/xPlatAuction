c24aaffef2b7728c120fa9d4277b20ad7f5e0edd
rakemen <rcmenon@indiana.edu>
2019-01-12 02:29:50 -0500

    Added backend project

---
+++ xPlatAuction.backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(xPlatAuction.backend.Startup))]
+
+namespace xPlatAuction.backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
